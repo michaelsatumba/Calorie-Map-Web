c65f722c960143dfcde065193a8cece972bde875
michaelsatumba <mjsatumba@gmail.com>
2022-03-16 13:01:00 -0700

    calories formula

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // alert('hello');
 
+const weight = document.getElementById('weight');
+
 mapboxgl.accessToken =
 	'pk.eyJ1IjoibWljaGFlbHNhdHVtYmFtYXBzIiwiYSI6ImNrdm15Z204YjAzajIyb3F2YWRkcjFuaWQifQ.X0jvoZunwGoLmk00y6CNog';
 
@@ -8,7 +10,7 @@ navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
 });
 
 function successLocation(position) {
-	console.log(position);
+	// console.log(position);
 	setupMap([position.coords.longitude, position.coords.latitude]);
 }
 
@@ -19,7 +21,7 @@ function errorLocation() {
 function setupMap(center) {
 	const map = new mapboxgl.Map({
 		container: 'map',
-		style: 'mapbox://styles/mapbox/streets-v11',
+		style: 'mapbox://styles/mapbox/streets-v9',
 		center: center,
 		zoom: 15,
 	});
@@ -27,48 +29,53 @@ function setupMap(center) {
 	const nav = new mapboxgl.NavigationControl();
 	map.addControl(nav);
 
-	var directions = new MapboxDirections({
+	const directions = new MapboxDirections({
 		accessToken: mapboxgl.accessToken,
+		unit: 'imperial',
+		profile: 'mapbox/walking',
+		alternatives: true,
+		congestion: true,
+		// geocoder:
+	});
+
+	map.on('load', function () {
+		directions.setOrigin('Oxnard'); // On load, set the origin.
+		directions.setDestination('Ventura'); // On load, set the destination.
+	});
+
+	directions.on('route', function (e) {
+		// console.log(e.route); // Logs the current route shown in the interface.
+		// console.log(e.route[0].distance); // Logs the distance in meters
+		// const distanceMeters = e.route[0].distance;
+		// const distanceMiles = distanceMeters * 0.000621;
+		// console.log(distanceMiles); // Logs the distance in miles.
+		// console.log(e.route[0].duration); // Logs the duration in seconds.
+		const seconds = e.route[0].duration;
+		const minutes = seconds / 60;
+		// console.log(minutes); // Logs the duration in minutes.
+		weight.addEventListener('input', () => {
+			// alert(weight.value);
+			// console.log(weight.value);
+			let x = weight.value;
+			let y = minutes;
+			// console.log(x);
+			// console.log(y);
+			let result = ((3.5 * (x * 0.45359237) * 3.5) / 200) * y;
+			document.getElementById('caloriesExpended').innerHTML =
+				Math.floor(result);
+		});
 	});
 
 	map.addControl(directions, 'top-left');
+
+	// Calories burned per minute = (MET x body weight in Kg x 3.5) ÷ 200
+	// Calories burned = ((MET x body weight in Kg x 3.5) ÷ 200) * minute
+	// MET walking = 3
+	// lbs to kg = lbs * 0.45359237
+	//EXAMPLE
+	/*
+		const weight = 160 
+		const walkingMins = 60
+        ((3 * (160 * 0.45359237) * 3.5 ) / 200) * 60
+	*/
 }
-
-// window.onload = function () {
-// 	findMe();
-// };
-
-/*
-// location logic
-function findMe() {
-	// const status = document.querySelector('.place');
-
-	function success(position) {
-		// console.log(position);
-		const latitude = position.coords.latitude;
-		const longitude = position.coords.longitude;
-		// alert(latitude + ' ' + longitude);
-		const geoURL =
-			'https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en';
-
-		fetch(geoURL)
-			.then((res) => res.json())
-			.then((data) => {
-				console.log(data);
-				// console.log(data.city);
-				// status.textContent = `In ${data.city}, ${data.countryName}`;
-				// document.querySelector('.currentPlace').textContent = `${data.city}`;
-			});
-	}
-
-	function error(showError) {
-		// status.textContent = `I'm going to find you`;
-		// document.querySelector(
-		// 	'.currentPlace'
-		// ).textContent = `I'm still going to find you`;
-		console.log(data);
-	}
-
-	navigator.geolocation.getCurrentPosition(success, error);
-}
-*/
