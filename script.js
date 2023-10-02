c9c88d8b70e29c1e5445ee07e27f7c65af5388a8
michaelsatumba <mjsatumba@gmail.com>
2023-10-02 12:08:30 -0700

    pwa

--- script.js
+++ script.js
@@ -251,3 +251,15 @@ function directionsToggle() {
 			'none';
 	}
 }
+
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', function() {
+	  navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+		// Registration was successful
+		console.log('ServiceWorker registration successful with scope: ', registration.scope);
+	  }, function(err) {
+		// registration failed :(
+		console.log('ServiceWorker registration failed: ', err);
+	  });
+	});
+  }
